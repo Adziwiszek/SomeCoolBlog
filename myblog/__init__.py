f48e88a3c73142d2f7c59876046a6995fc0ffe23
Adam Dziwiszek <adziwiszek1@gmail.com>
2024-07-21 15:55:29 +0200

    stuff from tutorial

--- myblog/__init__.py
+++ myblog/__init__.py
@@ -7,7 +7,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'myblog.sqlite'),
     )
 
     if test_config is None:
@@ -27,5 +27,15 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
